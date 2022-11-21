4f95d54d7ed002fdab7217d16a8669eff936acaa
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2022-11-21 08:14:27 -0300

    test: breakpoint testing improvements

--- src/components/Breakpoints/Breakpoint.test.js
+++ src/components/Breakpoints/Breakpoint.test.js
@@ -1,7 +1,12 @@
 import { BreakpointsSize, breakAt } from "./Breakpoints";
 
 describe("Breakpoint Service", () => {
-  it("should return the width as given value", () => {
-    expect(breakAt(BreakpointsSize.sm)).toEqual("@media (min-width: 576px)");
+  it.each([
+    [BreakpointsSize.sm],
+    [BreakpointsSize.md],
+    [BreakpointsSize.lg],
+    [BreakpointsSize.xl],
+  ])("should return the width as given value %ipx", (size) => {
+    expect(breakAt(size)).toEqual(`@media (min-width: ${size}px)`);
   });
 });
