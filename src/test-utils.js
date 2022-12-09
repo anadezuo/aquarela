c868493ec3acf47f08534eea1da5f1371641bb34
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2022-12-09 17:26:09 -0300

    feat: export userEvent library

--- src/test-utils.js
+++ src/test-utils.js
@@ -7,6 +7,7 @@ const TestProvider = ({ children }) => (
 );
 
 export * from "@testing-library/react";
+export { default as userEvent } from "@testing-library/user-event";
 
 const customRender = (ui, options) =>
   render(ui, { wrapper: TestProvider, ...options });
